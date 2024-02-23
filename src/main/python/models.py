100cf4f0c2ec4c544f9310d508168edbe788d6f9
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-02-23 19:50:24 +0100

    feat: Work with model

--- src/main/python/models.py
+++ src/main/python/models.py
@@ -1,10 +1,8 @@
 from datetime import datetime
-
-from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, DateProperty, config, db
-
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, DateProperty, config, db, One, \
+    ZeroOrOne
 
 config.DATABASE_URL = 'bolt://neo4j:12345678@localhost:7687'
-
 
 
 class HashNode(StructuredNode):
@@ -12,8 +10,37 @@ class HashNode(StructuredNode):
     path = StringProperty(index=True)
     hash = StringProperty(index=True)
     created_at = DateProperty(default=lambda: datetime.now())
-    lower = RelationshipTo('HashNode', 'LOWER')
-    upper = RelationshipTo('HashNode', 'UPPER')
+    lower = RelationshipTo('HashNode', 'LOWER', cardinality=ZeroOrOne)
+    upper = RelationshipTo('HashNode', 'UPPER', cardinality=ZeroOrOne)
+
+
+def find_node(root, name):
+    print(root, name)
+    if root is None:
+        return None
+    elif root.name == name:
+        return root
+    elif root.name > name:
+        return find_node(root.lower.single(), name)
+    else:
+        return find_node(root.upper.single(), name)
+
+
+def add_node(root, new_node):
+    if root is None:
+        return new_node
+    elif root.name > new_node.name:
+        son = root.lower.single()
+        if son is None:
+            root.lower.connect(new_node)
+        else:
+            add_node(son, new_node)
+    else:
+        son = root.upper.single()
+        if son is None:
+            root.upper.connect(new_node)
+        else:
+            add_node(son, new_node)
 
 
 if __name__ == "__main__":
@@ -40,4 +67,11 @@ if __name__ == "__main__":
     b.lower.connect(a)
     b.upper.connect(c)
 
+    d = HashNode()
+    d.name = 'd'
+    d.path = 'd'
+    d.hash = 'd'
+    d.save()
 
+    print(find_node(b, 'a'))
+    print(add_node(b, d))
